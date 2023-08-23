0555fa98c3e829874e4d044267e10913690992ff
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-23 07:10:05 +0000

    コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_id: book.id)
+    comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
     redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.find_by(book_id: book.id)
-    book_comment.destroy
+    BookComment.find(params[:id]).destroy
     redirect_to request.referer
   end
 
