525cdb35050ae53bffc3b984e63337f836923df4
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-23 04:26:23 +0000

    コメント機能　土台

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,21 @@
+class BookCommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_id: book.id)
+    comment.book_id = book.id
+    comment.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    book_comment = current_user.book_comments.find_by(book_id: book.id)
+    book_comment.destroy
+    redirect_to request.referer
+  end
+
+  private
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
