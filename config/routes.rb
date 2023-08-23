2a9e827aa7295745a34e253ef4661883a37f574d
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-23 08:31:48 +0000

    いいね機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
+
   devise_for :users
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
@@ -10,7 +11,5 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
