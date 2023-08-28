4a0a6baffaaae0dd3270819145000e402d2b92cd
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-28 12:15:08 +0000

    book閲覧数の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ApplicationRecord
   has_many :chats
   has_many :rooms, through: :user_rooms
 
+  has_many :view_counts, dependent: :destroy
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
