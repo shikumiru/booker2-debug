c3d0098e3c6aa83dce495354d08c9dbdb9ebbc83
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-28 06:42:53 +0000

    チャット　エラーメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render "index"
     end
   end
 
