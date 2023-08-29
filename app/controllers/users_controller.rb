2dabae454af009ff3eff8bb70f5dd48e82c48f03
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-29 10:59:49 +0000

    9b ユーザー指定日投稿数検索

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
     @last_week_book = @books.created_last_week
   end
 
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
+  end
+
   def index
     @users = User.all
     @book = Book.new
